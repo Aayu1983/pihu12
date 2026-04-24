5a14f96ac6236014cc04a5d1a7e6cb4307e7dae8
Aayush <aayush.singha01@@gmail.com>
2026-04-24 22:22:42 +0530

    add route table code

---
+++ import.tf
@@ -0,0 +1,27 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "4.14.0"
+    }
+  }
+
+  backend "azurerm" {
+    resource_group_name  = "myrg"
+    storage_account_name = "mystgs12345678"
+    container_name       = "mycontainer"
+    key                  = "prod-route-table.tfstate"
+  }
+}
+
+provider "azurerm" {
+  features {}
+  subscription_id = "5e2ade1f-9bb8-4b4f-874f-5fd49f66c2fa"
+}
+
+
+
+resource "azurerm_route_table" "route_table" {
+
+
+}

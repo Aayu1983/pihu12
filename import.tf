81130bcea7e099e24eab43d61a3803ad18cffa71
Aayush <aayush.singha01@@gmail.com>
2026-04-28 22:04:01 +0530

    add  code

--- import.tf
+++ import.tf
@@ -7,21 +7,19 @@ terraform {
   }
 
   backend "azurerm" {
-    resource_group_name  = "myrg"
-    storage_account_name = "stgpractic12345"
-    container_name       = "mycontainer"
-    key                  = "prod-route-table.tfstate"
+    resource_group_name  = "prodansh"
+    storage_account_name = "ansh09876"
+    container_name       = "aayu"
+    key                  = "appinside.tfstate"
   }
 }
 
 provider "azurerm" {
   features {}
-  subscription_id = "5e2ade1f-9bb8-4b4f-874f-5fd49f66c2fa"
+  subscription_id = "7f9ffab5-15de-40df-80c3-9f0897275cd1"
 }
 
-
-
 import {
-  to = azurerm_route_table.route_table
-  id = "/subscriptions/5e2ade1f-9bb8-4b4f-874f-5fd49f66c2fa/resourceGroups/myrg/providers/Microsoft.Network/routeTables/rt-Prodvnet-br"
+  to = azurerm_app_service_plan.app_block
+  id = "/subscriptions/7f9ffab5-15de-40df-80c3-9f0897275cd1/resourceGroups/prodansh/providers/microsoft.insights/components/appinsight"
 }
